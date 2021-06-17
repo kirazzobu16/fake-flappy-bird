aa7d03badf0c50f5958786e6dd883c6345afbfa2
kirazzobu16 <37017093+kirazzobu16@users.noreply.github.com>
2021-06-17 17:37:07 +0300

    prototip yapıldı

---
+++ fake flappy bird/Assets/script/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public int Score;
+    public Text ScoreText;
+    void Start()
+    {
+        Score = 0;
+        ScoreText.text = Score.ToString();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    public void UpdateScore()
+    {
+        Score++;
+        ScoreText.text = Score.ToString();
+    }
+}
---
+++ fake flappy bird/Assets/script/Hareket.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hareket : MonoBehaviour
+{   
+    public float eklenenaci;
+    float egim = 45f;
+    public GameManager managergame;
+      bool oyunbitti = true;
+   
+    private void Start()
+    {
+        
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, egim);
+     
+    }
+
+    void Update()
+    {
+        if (oyunbitti == true)
+        {
+            fırsttransform();
+            if (egim <= -45)
+            {
+
+                eklenenaci = 90f;
+
+            }
+            else if (egim >= 45)
+            {
+                eklenenaci = -90f;
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, egim + eklenenaci);
+                egim = egim + eklenenaci;
+            }
+        }
+    }
+    private void fırsttransform()
+    {
+        Vector3 position = this.transform.position;
+        
+
+        if (egim<=-45)
+        {
+            position.x += 0.005f;
+            position.y -= 0.005f;
+            this.transform.position = position;
+        }
+        if(egim>=45)
+        {
+            position.x += 0.005f;
+            position.y += 0.005f;
+            this.transform.position = position;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag== "SkorAlani")
+        {
+            managergame.UpdateScore();
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        oyunbitti = false;
+        SceneManager.LoadScene(0);
+    }
+
+
+
+
+
+
+
+
+}
